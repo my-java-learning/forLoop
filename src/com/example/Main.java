9df6704ff7b65f8a067ab6aca88857e7e3425dbf
Qsior <lukaszbojes@gmail.com>
2017-11-05 22:37:55 +0100

    Modified exercise

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("Loop " + i + " hello!");
         }
 
-        for(int interestRate = 2; interestRate <= 8; interestRate++){
+        for(int interestRate = 8; interestRate >= 2; interestRate--){
             System.out.println("10,000 at " + interestRate + "% interest = " + String.format("%.2f",calculateInterest(10000.0, interestRate)));
         }
 
